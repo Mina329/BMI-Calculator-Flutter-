2a5f9bc0c3912303023fd0fc3738c1b54175d9cf
Mina Emil <minaemil329@gmail.com>
2022-02-15 10:52:41 +0200

    Add Constants File

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const bottombuttoncolor =Color(0xFFEB1555) ;
+const activebuttoncolor = Color(0xFF1D1E33);
+const inactivebuttoncolor =Color(0xFF111328);
+const BottomContainerHeight = 80.0;
+
+const labelTextStyle =TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+)
